7ff2e23871e679018c71409e4bc4633c7e4d97bf
ryanda9910 <ryandaaldo@gmail.com>
2022-05-17 13:17:27 +0700

    feat(sentry):add sentry tracking error

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -2,11 +2,14 @@ import React from "react";
 import "../../Styles/MainPage.scss";
 
 export const MainPage = () => {
+	const throwNewError=()=>{
+		throw new Error('This is an error from sentry tutorials')
+	}
 	return (
 		<>
 			<div className="mainPage">
 				<h1>Hello World</h1>
-        <button>Click Here</button>
+        <button onClick={throwNewError}>Click Here</button>
 			</div>
 		</>
 	);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+Sentry.init({
+  dsn: "https://041768eae2104575b92f0f873935b386@o1250105.ingest.sentry.io/6413971",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 root.render(
   <React.StrictMode>
     <App />
